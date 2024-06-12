762fa735c279490090eed73eeed6eaf5d572f807
Wambui <nderituwambui93@gmail.com>
2024-06-13 02:28:50 +0300

    updating status

--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-
-from bool_functions import return_true
-
-def test_return_true():
-    '''in bool_functions, function "return_true" returns True.'''
-    assert return_true() == True
+# In bool_functions.py
+def return_true():
+    return True
